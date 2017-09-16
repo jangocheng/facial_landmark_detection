f0efc2309306e5c66a128c614fe7eb0ff67522b4
WeiJi Hsiao <weiji.hsiao@gmail.com>
2017-09-16 23:57:50 +0800

    visualization with tensorboard

--- inference.py
+++ inference.py
@@ -19,76 +19,156 @@ FC2_SIZE = 500
 
 
 def get_weights(size):
-    return tf.get_variable('weights', size, initializer=tf.truncated_normal_initializer(stddev=0.1))
+    return tf.Variable(tf.truncated_normal(size, stddev=0.1))
 
 
 def get_biases(size):
-    return tf.get_variable('biases', size, initializer=tf.constant_initializer(0.0))
+    return tf.Variable(tf.constant(0.0, shape=size))
+
+
+def variable_summaries(var, name):
+    with tf.name_scope('summaries'):
+        tf.summary.histogram(name, var)
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean/' + name, mean)
+        stddev = tf.sqrt(tf.reduce_mean(tf.sqrt(var - mean)))
+        tf.summary.scalar('stddev/' + name, stddev)
+
+
+def conv_layer(input_tensor, input_channels, filter_length, filter_width, filter_depth, layer_name):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            conv_weights = get_weights(
+                [filter_length, filter_width, input_channels, filter_depth])
+            variable_summaries(conv_weights, layer_name + '/weights')
+        with tf.name_scope('biases'):
+            conv_biases = get_biases([filter_depth])
+            variable_summaries(conv_biases, layer_name + '/biases')
+        with tf.name_scope('convolution'):
+            conv = tf.nn.conv2d(input_tensor, conv_weights, strides=[
+                                1, 1, 1, 1], padding='SAME')
+            relu = tf.nn.relu(tf.nn.bias_add(conv, conv_biases))
+            tf.summary.histogram(layer_name + '/convolution', relu)
+        return relu
+
+
+def max_pool_layer(input_tensor, layer_name):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('max_pool'):
+            pool = tf.nn.max_pool(input_tensor, ksize=[1, 2, 2, 1], strides=[
+                                  1, 2, 2, 1], padding='SAME')
+            tf.summary.histogram(layer_name + '/max_pool', pool)
+        return pool
+
+
+def fc_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            weights = get_weights([input_dim, output_dim])
+            variable_summaries(weights, layer_name + '/weights')
+        with tf.name_scope('biases'):
+            biases = get_biases([output_dim])
+            variable_summaries(biases, layer_name + '/biases')
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram(layer_name + '/pre_activations', preactivate)
+        activations = act(preactivate, name='activation')
+        tf.summary.histogram(layer_name + '/activations', activations)
+        return activations
 
 
 def inference(input_tensor, train):
-    with tf.variable_scope('layer1-conv1'):
-        conv1_weights = get_weights(
-            [CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEEP])
-        conv1_biases = get_biases([CONV1_DEEP])
+    conv1 = conv_layer(input_tensor, NUM_CHANNELS, CONV1_SIZE,
+                       CONV1_SIZE, CONV1_DEEP, 'layer1-conv1')
+    pool1 = max_pool_layer(conv1, 'layer2-pool1')
 
-        conv1 = tf.nn.conv2d(input_tensor, conv1_weights,
-                             strides=[1, 1, 1, 1], padding='SAME')
-        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+    conv2 = conv_layer(pool1, CONV1_DEEP, CONV2_SIZE,
+                       CONV2_SIZE, CONV2_DEEP, 'layer3-conv2')
+    pool2 = max_pool_layer(conv2, 'layer4-pool2')
 
-    with tf.variable_scope('layer2-pool1'):
-        pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[
-                               1, 2, 2, 1], padding='SAME')
-
-    with tf.variable_scope('layer3-conv2'):
-        conv2_weights = get_weights(
-            [CONV2_SIZE, CONV2_SIZE, CONV1_DEEP, CONV2_DEEP])
-        conv2_biases = get_biases([CONV2_DEEP])
-
-        conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[
-                             1, 1, 1, 1], padding='SAME')
-        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
-
-    with tf.variable_scope('layer4-pool2'):
-        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[
-                               1, 2, 2, 1], padding='SAME')
-
-    with tf.variable_scope('layer5-conv3'):
-        conv3_weights = get_weights(
-            [CONV3_SIZE, CONV3_SIZE, CONV2_DEEP, CONV3_DEEP])
-        conv3_biases = get_biases([CONV3_DEEP])
-
-        conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[
-                             1, 1, 1, 1], padding='SAME')
-        relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
-
-    with tf.variable_scope('layer6-pool3'):
-        pool3 = tf.nn.max_pool(relu3, ksize=[1, 2, 2, 1], strides=[
-                               1, 2, 2, 1], padding='SAME')
+    conv3 = conv_layer(pool2, CONV2_DEEP, CONV3_SIZE,
+                       CONV3_SIZE, CONV3_DEEP, 'layer5-conv3')
+    pool3 = max_pool_layer(conv3, 'layer6-pool3')
 
     pool_shape = pool3.get_shape().as_list()
     nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
     reshaped = tf.reshape(pool3, [-1, nodes])
 
-    with tf.variable_scope('layer7-fc1'):
-        fc1_weights = get_weights([nodes, FC1_SIZE])
-        fc1_biases = get_biases([FC1_SIZE])
+    fc1 = fc_layer(reshaped, nodes, FC1_SIZE, 'layer7-fc1')
+    if train:
+        fc1 = tf.nn.dropout(fc1, 0.5)
 
-        fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
-        if train:
-            fc1 = tf.nn.dropout(fc1, 0.5)
+    fc2 = fc_layer(fc1, FC1_SIZE, FC2_SIZE, 'layer8-fc2')
+    if train:
+        fc2 = tf.nn.dropout(fc2, 0.5)
 
-    with tf.variable_scope('layer8-fc2'):
-        fc2_weights = get_weights([FC1_SIZE, FC2_SIZE])
-        fc2_biases = get_biases([FC2_SIZE])
+    fc3 = fc_layer(fc2, FC2_SIZE, NUM_LANDMARKS *
+                   2, 'layer9-fc3', act=tf.identity)
 
-        fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
-        if train:
-            fc2 = tf.nn.dropout(fc2, 0.5)
+    return fc3
 
-    with tf.variable_scope('layer9-fc3'):
-        fc3_weights = get_weights([FC2_SIZE, NUM_LANDMARKS * 2])
-        fc3_biases = get_biases([NUM_LANDMARKS * 2])
-        targets = tf.matmul(fc2, fc3_weights) + fc3_biases
+    # with tf.variable_scope('layer1-conv1'):
+    #     conv1_weights = get_weights(
+    #         [CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEEP])
+    #     conv1_biases = get_biases([CONV1_DEEP])
 
-    return targets
+    #     conv1 = tf.nn.conv2d(input_tensor, conv1_weights,
+    #                          strides=[1, 1, 1, 1], padding='SAME')
+    #     relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    # with tf.variable_scope('layer2-pool1'):
+    #     pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[
+    #                            1, 2, 2, 1], padding='SAME')
+
+    # with tf.variable_scope('layer3-conv2'):
+    #     conv2_weights = get_weights(
+    #         [CONV2_SIZE, CONV2_SIZE, CONV1_DEEP, CONV2_DEEP])
+    #     conv2_biases = get_biases([CONV2_DEEP])
+
+    #     conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[
+    #                          1, 1, 1, 1], padding='SAME')
+    #     relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+
+    # with tf.variable_scope('layer4-pool2'):
+    #     pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[
+    #                            1, 2, 2, 1], padding='SAME')
+
+    # with tf.variable_scope('layer5-conv3'):
+    #     conv3_weights = get_weights(
+    #         [CONV3_SIZE, CONV3_SIZE, CONV2_DEEP, CONV3_DEEP])
+    #     conv3_biases = get_biases([CONV3_DEEP])
+
+    #     conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[
+    #                          1, 1, 1, 1], padding='SAME')
+    #     relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
+
+    # with tf.variable_scope('layer6-pool3'):
+    #     pool3 = tf.nn.max_pool(relu3, ksize=[1, 2, 2, 1], strides=[
+    #                            1, 2, 2, 1], padding='SAME')
+
+    # pool_shape = pool3.get_shape().as_list()
+    # nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+    # reshaped = tf.reshape(pool3, [-1, nodes])
+
+    # with tf.variable_scope('layer7-fc1'):
+    #     fc1_weights = get_weights([nodes, FC1_SIZE])
+    #     fc1_biases = get_biases([FC1_SIZE])
+
+    #     fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
+    #     if train:
+    #         fc1 = tf.nn.dropout(fc1, 0.5)
+
+    # with tf.variable_scope('layer8-fc2'):
+    #     fc2_weights = get_weights([FC1_SIZE, FC2_SIZE])
+    #     fc2_biases = get_biases([FC2_SIZE])
+
+    #     fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
+    #     if train:
+    #         fc2 = tf.nn.dropout(fc2, 0.5)
+
+    # with tf.variable_scope('layer9-fc3'):
+    #     fc3_weights = get_weights([FC2_SIZE, NUM_LANDMARKS * 2])
+    #     fc3_biases = get_biases([NUM_LANDMARKS * 2])
+    #     targets = tf.matmul(fc2, fc3_weights) + fc3_biases
+
+    # return targets
--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import inference
 TRAIN_FILE = 'kaggle/training.csv'
 TEST_FILE = 'kaggle/test.csv'
 SAVE_PATH = 'model'
+SUMMARY_PATH = 'log'
 
 VALIDATION_SIZE = 100
 BATCH_SIZE = 64
@@ -15,16 +16,17 @@ TRAINING_STEPS = 2000
 
 
 def train():
-    x = tf.placeholder(
-        tf.float32,
-        [None, inference.IMAGE_SIZE, inference.IMAGE_SIZE, inference.NUM_CHANNELS],
-        name='x-input'
-    )
-    y_ = tf.placeholder(
-        tf.float32,
-        [None, inference.NUM_LANDMARKS * 2],
-        name='y-input'
-    )
+    with tf.name_scope('input'):
+        x = tf.placeholder(
+            tf.float32,
+            [None, inference.IMAGE_SIZE, inference.IMAGE_SIZE, inference.NUM_CHANNELS],
+            name='x-input'
+        )
+        y_ = tf.placeholder(
+            tf.float32,
+            [None, inference.NUM_LANDMARKS * 2],
+            name='y-input'
+        )
 
     y = inference.inference(x, True)
     global_step = tf.Variable(0, trainable=False)
@@ -33,32 +35,52 @@ def train():
         MOVING_AVERAGE_DECAY, global_step)
     variable_averages_op = variable_averages.apply(tf.trainable_variables())
 
-    rmse = tf.sqrt(tf.reduce_mean(tf.square(y - y_)))
-    total_error = tf.reduce_sum(tf.square(y - tf.reduce_mean(y)))
-    unexplained_error = tf.reduce_sum(tf.square(y - y_))
-    r_squared = 1 - tf.divide(unexplained_error, total_error)
+    with tf.name_scope('rmse'):
+        rmse = tf.sqrt(tf.reduce_mean(tf.square(y - y_)))
+        tf.summary.scalar('rmse', rmse)
+    with tf.name_scope('r_squre'):
+        total_error = tf.reduce_sum(tf.square(y - tf.reduce_mean(y)))
+        unexplained_error = tf.reduce_sum(tf.square(y - y_))
+        r_square = 1 - tf.divide(unexplained_error, total_error)
+        tf.summary.scalar('r_squre', r_square)
 
-    train_step = tf.train.AdamOptimizer(
-        LEARNING_RATE).minimize(rmse, global_step=global_step)
-    train_op = tf.group(train_step, variable_averages_op, name='train')
+    with tf.name_scope('train'):
+        train_step = tf.train.AdamOptimizer(
+            LEARNING_RATE).minimize(rmse, global_step=global_step)
+        train_op = tf.group(train_step, variable_averages_op, name='train')
 
+    merged = tf.summary.merge_all()
     # saver = tf.train.Saver()
 
     with tf.Session() as sess:
+        train_summary_writer = tf.summary.FileWriter(
+            SUMMARY_PATH + '/train', sess.graph)
+        valid_summary_writer = tf.summary.FileWriter(
+            SUMMARY_PATH + '/valid', sess.graph)
         tf.global_variables_initializer().run()
         data = datasets.kaggle_data(TRAIN_FILE, 0.3)
         train_data = data['train']
         valid_data = data['valid']
         for i in range(TRAINING_STEPS):
             xs, ys = train_data.next_batch(BATCH_SIZE)
-            _, rmse_value, r2_score, step = sess.run(
-                [train_op, rmse, r_squared, global_step], feed_dict={x: xs, y_: ys})
+            # _, rmse_value, r2_score, step = sess.run(
+            #     [train_op, rmse, r_square, global_step], feed_dict={x: xs, y_: ys})
+            summary, _ = sess.run([merged, train_op],
+                                  feed_dict={x: xs, y_: ys})
+            train_summary_writer.add_summary(summary, i)
 
-            if i % 100 == 0:
-                rmse_valid, r2_valid = sess.run([rmse, r_squared], feed_dict={
-                                                x: valid_data.images, y_: valid_data.targets})
-                print('Step {:d}: rmse(train): {:.6f}, r_squared(train): {:.6f}, rmse(valid):{:.6f},r_squared(valid):{:.6f}'.format(
-                    step, rmse_value, r2_score, rmse_valid, r2_valid))
+            if i % 10 == 0:
+                summary = sess.run(merged, feed_dict={
+                                   x: valid_data.images, y_: valid_data.targets})
+                valid_summary_writer.add_summary(summary, i)
+        train_summary_writer.close()
+        valid_summary_writer.close()
+
+        # if i % 100 == 0:
+        #     rmse_valid, r2_valid = sess.run([rmse, r_square], feed_dict={
+        #                                     x: valid_data.images, y_: valid_data.targets})
+        #     print('Step {:d}: rmse(train): {:.6f}, r_square(train): {:.6f}, rmse(valid):{:.6f},r_squared(valid):{:.6f}'.format(
+        #         step, rmse_value, r2_score, rmse_valid, r2_valid))
 
 
 if __name__ == '__main__':
